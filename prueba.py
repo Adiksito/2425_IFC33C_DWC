677929c5e6ba251c837f60b0ee41891b255ab37c
“Alan” <adamskialan721@gmail.com>
2024-10-03 16:04:33 +0200

    hasta 03102024

---
+++ prueba.py
@@ -0,0 +1,32 @@
+if 5 > 2:
+print("Esto da error")
+
+
+if 5 > 2:
+ print("Esto tampoco")
+
+x = 5
+y = "Prueba"
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)
+
+2myvar = "Prueba"
+my-var = "Prueba"
+my var = "Prueba"
+x, y, z = "Hola", "Adios", "Hasta luego"
+
+
+x = y = z = "Orange"
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+
+x = 3    # int
+y = 5.8  # float
+z = 1j   # complex
